be2c51a52f3ff4198af98f83b298af55d928553d
BKN46 <1149887546@qq.com>
2023-03-07 16:59:12 +0800

    chatgpt streamed

--- toogle/plugins/openai.py
+++ toogle/plugins/openai.py
@@ -1,4 +1,6 @@
+import json
 import requests
+import time
 from requests.exceptions import ReadTimeout
 
 from toogle.configs import config, interval_limiter
@@ -25,7 +27,7 @@ class GetOpenAIConversation(MessageHandler):
         message_content = message.message.asDisplay()[4:].strip()
         try:
             # res = GetOpenAIConversation.get_completion(message_content)
-            res = GetOpenAIConversation.get_chat(message_content)
+            res = GetOpenAIConversation.get_chat_stream(message_content)
             return MessageChain.plain(res)
         except ReadTimeout as e:
             return MessageChain.plain("请求OpenAI GPT模型超时，请稍后尝试")
@@ -66,3 +68,27 @@ class GetOpenAIConversation(MessageHandler):
             return res.json()["choices"][0]["message"]["content"].strip()
         except Exception as e:
             return res.text
+
+    @staticmethod
+    def get_chat_stream(text: str, max_time=10) -> str:
+        path = "/chat/completions"
+        body = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": text}],
+            "stream": True,
+        }
+        res = requests.post(url + path, headers=header, json=body, proxies=proxies, stream=True, timeout=5)
+        res_text = ''
+        start_time = time.time()
+        for line in res.iter_lines():
+            # filter out keep-alive new lines
+            if line:
+                decoded_line = line.decode('utf-8')
+                if decoded_line.startswith("data:") and not decoded_line.endswith("[DONE]"):
+                    data = json.loads(decoded_line[5:].strip())
+                    if 'content' in data['choices'][0]['delta']:
+                        res_text += data['choices'][0]['delta']['content']
+            if time.time() - start_time > max_time:
+                res_text += "\n[由于时长限制后续生成直接截断]"
+                break
+        return res_text.strip()
