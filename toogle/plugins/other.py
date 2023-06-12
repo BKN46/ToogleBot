263b9708ac8947193efec5ee554b411cd1ff4f88
BKN46 <1149887546@qq.com>
2023-06-12 17:44:44 +0800

    diablo4 tracker

--- toogle/plugins/other.py
+++ toogle/plugins/other.py
@@ -1,8 +1,11 @@
+import datetime
 import io
 import pickle
 import random
+import time
 
 import PIL.Image
+import requests
 
 from toogle.message import Image, MessageChain, Plain
 from toogle.message_handler import MessageHandler, MessagePack
@@ -280,3 +283,41 @@ class ToogleCSServer(MessageHandler):
         )
         return MessageChain.plain(help_str, quote=message.as_quote())
 
+
+class Diablo4Tracker(MessageHandler):
+    name = "D4 event tracker"
+    trigger = r"^d4boss(\snoreply|)$"
+    thread_limit = True
+    readme = "暗黑破坏神4跟踪"
+
+    async def ret(self, message: MessagePack) -> MessageChain:
+        content = message.message.asDisplay()[6:].strip()
+        no_reply = False
+        if content.startswith("noreply"):
+            no_reply = True
+
+        try:
+            res = requests.get("https://diablo4.life/api/trackers/worldBoss/reportHistory?name=&limit=20").json()['reports']
+        except Exception as e:
+            if no_reply:
+                return MessageChain([])
+            return MessageChain.plain("请求失败", quote=message.as_quote())
+
+        for item in res:
+            now_time = int(time.time() * 1000)
+            left_time = ((item['spawnTime'] - now_time) / 1000 / 60)
+            if left_time > 0 and left_time < 10 and item['status'] == 'validated':
+                spawn_time_text = datetime.datetime.fromtimestamp(item['spawnTime'] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+                alert_text = (
+                    f"注意世界Boss即将于{spawn_time_text}刷新\n"
+                    f"BOSS：{item['name']}\n位于：{item['location']}\n"
+                )
+                return MessageChain.plain(alert_text)
+        
+        if no_reply:
+            return MessageChain([])
+        res = [x for x in res if x['status'] == 'validated']
+        spawn_time_text = datetime.datetime.fromtimestamp(res[0]['spawnTime'] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+        next_time = datetime.datetime.fromtimestamp(res[0]['spawnTime'] / 1000 + 60 * 15 + 3600 * 5).strftime("%Y-%m-%d %H:%M:%S")
+        next_time_2 = datetime.datetime.fromtimestamp(res[0]['spawnTime'] / 1000 + 60 * 15 + 3600 * 8).strftime("%Y-%m-%d %H:%M:%S")
+        return MessageChain.plain(f"暂无世界Boss刷新信息\n最近一次刷新为 {spawn_time_text}\n下次刷新可能在 {next_time} - {next_time_2}", quote=message.as_quote())
