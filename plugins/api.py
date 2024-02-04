fe677180748f3ce04bf6946e88c055ea8ac51d84
BKN46 <1149887546@qq.com>
2024-02-04 17:35:23 +0800

    api update

--- plugins/api.py
+++ plugins/api.py
@@ -1,5 +1,7 @@
 import nonebot
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from toogle.nonebot2_adapter import bot_send_message
 
 app: FastAPI = nonebot.get_app()
 
@@ -7,3 +9,11 @@ app: FastAPI = nonebot.get_app()
 @app.get("/api")
 async def custom_api():
     return {"message": "Hello, world!"}
+
+
+@app.post("/send")
+async def send_message(request: Request):
+    data = await request.json()
+    group, msg = data["group"], data["message"]
+    await bot_send_message(group, msg)
+    return {"message": "success"}
