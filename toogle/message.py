7812b7009b9d81b89e24eb0d25daee7613d38ab2
BKN46 <1149887546@qq.com>
2024-06-05 10:30:45 +0800

    minor fix

--- toogle/message.py
+++ toogle/message.py
@@ -82,6 +82,9 @@ class Plain(Element):
     def __init__(self, text: str) -> None:
         self.text = text
 
+    def __str__(self) -> str:
+        return self.text
+
     def asDisplay(self) -> str:
         return self.text
 
