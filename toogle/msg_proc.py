a31301e86940f612ae5eacb00a38602566fdff1d
BKN46 <1149887546@qq.com>
2024-06-18 14:26:03 +0800

    gpt censor

--- toogle/msg_proc.py
+++ toogle/msg_proc.py
@@ -3,11 +3,12 @@ import os
 import threading
 
 from toogle.economy import get_balance, give_balance
-from toogle.message import Image, MessageChain, Plain
+from toogle.message import At, Image, MessageChain, Plain
 from toogle.message_handler import MESSAGE_HISTORY, MessagePack
 from toogle.nonebot2_adapter import bot_send_message
 from toogle.configs import config
-from toogle.utils import SETU_RECORD_PATH, detect_pic_nsfw
+from toogle.utils import SETU_RECORD_PATH, detect_pic_nsfw, print_err
+from toogle.plugins.openai import gpt_censor, GetOpenAIConversation
 
 POST_PROC_LOCK = threading.Lock()
 
@@ -29,7 +30,7 @@ def update_setu_record(group_id, member_id, cnt):
 
 
 async def chat_earn(message_pack: MessagePack):
-    pics = message_pack.message.get(Image)
+    pics = message_pack.message.get(Image, ignore_forawrd=True)
     if get_balance(message_pack.member.id) < 15:
         if len(MessageChain(message_pack.message.get(Plain)).asDisplay()) >= 10:
             give_balance(message_pack.member.id, 1)
@@ -37,6 +38,8 @@ async def chat_earn(message_pack: MessagePack):
     if pics:
         if str(message_pack.group.id) in config.get('NSFW_LIST', []):
             cnt = 0
+            if len(pics) > 5:
+                pics = pics[:5]
             for pic in pics:
                 score, repeat = detect_pic_nsfw(pic.getBytes(), output_repeat=True) # type: ignore
                 if not repeat and score >= 0.25:
@@ -45,3 +48,26 @@ async def chat_earn(message_pack: MessagePack):
                 give_balance(message_pack.member.id, cnt)
                 update_setu_record(message_pack.group.id, message_pack.member.id, cnt)
                 MESSAGE_HISTORY.add(f"setu_{message_pack.group.id}", message_pack)
+
+
+async def chat_cencor(message_pack: MessagePack):
+    if str(message_pack.group.id) in config.get('CENSOR_LIST', []):
+        if not message_pack.message.get(Plain) or len(message_pack.message.asDisplay()) < 4:
+            return
+        MESSAGE_HISTORY.add(f"censor_{message_pack.group.id}", message_pack)
+        remain_history = MESSAGE_HISTORY.get(f"censor_{message_pack.group.id}", windows=10)
+        if len(remain_history) >= 10:
+            try:
+                score, content, cost = gpt_censor(remain_history) # type: ignore
+            except Exception as e:
+                print_err(e, GetOpenAIConversation, message_pack)
+                return
+            if score >= 30:
+                await bot_send_message(
+                    int(message_pack.group.id),
+                    MessageChain.create([
+                        Plain(f"鉴证警报 鉴证警报: {content}\n"),
+                        *[At(int(x)) for x in config.get('ADMIN_LIST', [])],
+                    ]),
+                )
+            MESSAGE_HISTORY.delete(f"censor_{message_pack.group.id}")
