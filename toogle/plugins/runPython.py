e75bdb7a84da947b766c399ba7020591e9645253
BKN46 <1149887546@qq.com>
2024-10-08 10:10:11 +0800

    run lua

--- toogle/plugins/runPython.py
+++ toogle/plugins/runPython.py
@@ -74,3 +74,50 @@ class RunPython(MessageHandler):
         except Exception as e:
             running_res = repr(e)
         return running_res
+
+
+class RunLua(MessageHandler):
+    name = "Lua解释器"
+    trigger = r"^#lua#"
+    readme = "简易lua脚本运行"
+    price = 5
+    
+    async def ret(self, message: MessagePack) -> MessageChain:
+        text = message.message.asDisplay()[5:]
+        running_res: str = RunLua.get_exec(text, str(message.member.id) in config["SUPERUSERS"])
+        return MessageChain.create([Plain(running_res)])
+
+    @staticmethod
+    def sensor_extract(text):
+        return [
+            x
+            for x in [
+                "os.",
+                "io.",
+                "file",
+            ]
+            if x in text
+        ]
+
+    @staticmethod
+    @set_timeout(2, handle_TLE)
+    def get_exec(text, admin=False) -> str:
+        try:
+            sensors = RunLua.sensor_extract(text)
+            if any(sensors):
+                if admin:
+                    raise Exception(f"含有危险词: {sensors}")
+                raise Exception("含有危险词!")
+            with RunPython.stdoutIO() as s:
+                import lupa
+                lua = lupa.LuaRuntime(unpack_returned_tuples=True)
+                lua.globals()['print'] = print
+                lua.execute(text)
+                running_res = s.getvalue()
+                if len(running_res.split("\n")) > 20:
+                    raise Exception("输出行数超长!")
+                elif len(running_res) > 1000:
+                    raise Exception("输出字数过多!")
+        except Exception as e:
+            running_res = repr(e)
+        return running_res
